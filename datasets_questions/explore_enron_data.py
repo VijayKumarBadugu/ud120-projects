9b5c16a10c277239e1fecda30c12fdf997d94290
badugv1 <Vijay.Badugu@emc.com>
2017-03-31 17:29:11 -0400

    datasets_questions done

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,28 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+print len(enron_data.keys())
+print len(enron_data.values()[0])
+poi = 0
+for k,v in enron_data.items():
+	if enron_data[k]["poi"] is True:
+		poi = poi +1
+print poi
 
-
+poi_names = open("../final_project/poi_names.txt").read().split('\n')
+poi_y = [name for name in poi_names if "(y)" in name]
+poi_n = [name for name in poi_names if "(n)" in name]
+print("poi_names_count:", len(poi_y+poi_n))
+print enron_data["PRENTICE JAMES"]["total_stock_value"]	
+#print enron_data["COLWELL WESLEY"]
+print enron_data["SKILLING JEFFREY K"]
+poi = 0
+for k in enron_data.keys():
+	if enron_data[k]["salary"] != 'NaN':
+		poi = poi +1
+print poi
+poi =0
+for k in enron_data.keys():
+        if enron_data[k]["email_address"] != 'NaN':
+                poi = poi +1
+print poi		
